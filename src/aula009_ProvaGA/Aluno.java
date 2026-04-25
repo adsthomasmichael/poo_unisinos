85bb5a30e0d84e98dfee056e7fde534e9526a83e
Michael Thomas <ads.thomasmichael@gmail.com>
2026-04-25 18:14:07 -0300

    Clase Aluno criada

---
+++ src/aula009_ProvaGA/Aluno.java
@@ -0,0 +1,55 @@
+package aula009_ProvaGA;
+
+public class Aluno {
+	
+	/* 	Crie uma classe Aluno. Um aluno possui um um nome, uma matrícula e um coeficiente de
+		rendimento (valor entre 0.0 e 10.0). Crie os construtores que desejar, os métodos de acesso e um método
+		que imprime as informações do aluno. */
+
+	private String nome;
+	private String matricula;
+	private double coeficienteRend;
+	
+	public Aluno(String nome, String matricula, double coeficienteRend) {
+		setNome(nome);
+		setMatricula(matricula);
+		this.coeficienteRend = coeficienteRend;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		if( nome != null && nome.trim().isEmpty()) {
+			this.nome = nome;
+		}
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		if( matricula != null && matricula.trim().isEmpty()) {
+			this.matricula = matricula;
+		}
+	}
+
+	public double getCoeficienteRend() {
+		return coeficienteRend;
+	}
+
+	public void setCoeficienteRend(double coeficienteRend) {
+		this.coeficienteRend = coeficienteRend;
+	}
+	
+	public void exibirInformacoes() {
+		System.out.println("=== ALUNO ===\n");
+		System.out.println("Nome               : "+ getNome());
+		System.out.println("Matricula          : "+ getMatricula());
+		System.out.println("Coef. de rendimento: "+ getCoeficienteRend());
+	}
+	
+	
+}
