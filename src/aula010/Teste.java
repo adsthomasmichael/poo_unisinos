bb466daca5c77c8624c25c2fa18d593a09703352
Michael Thomas <ads.thomasmichael@gmail.com>
2026-05-04 16:39:55 -0300

    Conclusão classe Teste

--- src/aula010/Teste.java
+++ src/aula010/Teste.java
@@ -1,5 +1,7 @@
 package aula010;
 
+import com.utils.Teclado;
+
 public class Teste {
 
 	public static void main(String[] args) {
@@ -24,9 +26,24 @@ public class Teste {
 		System.out.println("Cor: "+m01.getCor());
 		System.out.printf("Peso: %.2fkg", m01.getPeso());
 		System.out.printf("\nPreço: R$%.2f", m01.getPreco());
-		System.out.printf("\nPreço final: R$%.2f", m01.calculaPrecoFinal());
+		System.out.printf("\nPreço final: R$%.2f\n", m01.calculaPrecoFinal());
 	
-	
+		//Crie, no método main, uma Pessoa p.
+		Pessoa p;
+		
+		//conforme a opção do usuário (o usuário digita via teclado), instancie a Pessoa para um Programador (caso digite 1) ou um Aluno (caso digite 2).
+		int opcao = Teclado.leInt("Escolha a opção: \n"
+				+ "1: Programador\n"
+				+ "2: Aluno");
+		/* se a Pessoa for um Programador, imprima sua linguagem de programação preferida. Caso a Pessoa seja um Aluno, imprima a nota referente a este aluno.*/
+		if(opcao == 1) {
+			p = new Programador("Michael Thomas", 42, "Java");
+		}
+		else {
+			p = new Aluno("Luisito", 16, 8.7);
+		}
+		p.imprimeDados();
+		
 	
 	
 	}
