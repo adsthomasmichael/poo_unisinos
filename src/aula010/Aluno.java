cb22e487dca9a571c31160ec95f483d35d1712dd
Michael Thomas <ads.thomasmichael@gmail.com>
2026-05-04 15:00:13 -0300

    Classe Aluno

---
+++ src/aula010/Aluno.java
@@ -0,0 +1,34 @@
+package aula010;
+
+public class Aluno extends Pessoa {
+	/* crie a classe Aluno, que herda Pessoa e possui um atributo nota. Crie os métodos de acesso para este atributo. */
+	
+	protected double nota;
+
+	public Aluno() {
+		super();
+		
+	}
+
+	public Aluno(String nome, int idade, double nota) {
+		super(nome, idade);
+		this.nota = nota;
+	}
+
+	public double getNota() {
+		return nota;
+	}
+
+	public void setNota(double nota) {
+		this.nota = nota;
+	}
+	
+	public void imprimeDados() {
+		System.out.println("=== ALUNO ===\n");
+		super.imprimeDados();
+        System.out.println("NOta: " + getNota());
+        
+    }
+	
+
+}
