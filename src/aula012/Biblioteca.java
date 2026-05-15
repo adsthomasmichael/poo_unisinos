077e682e9c409f710cd0c6e57be1958468d61085
adsthomasmichael <ads.thomasmichael@gmail.com>
2026-05-15 17:03:59 -0300

    Atualização classe Biblioteca.

---
+++ src/aula012/Biblioteca.java
@@ -0,0 +1,61 @@
+package aula012;
+
+import java.util.Arrays;
+
+public class Biblioteca {
+    /* Crie uma classe Biblioteca, que possui um array de Livro. Crie os métodos de acesso e o toString para esta classe.
+    Nesta classe, ainda, faça o que se pede:*/
+
+    protected Livro[] arrayLivro;
+
+    //  A - no construtor, receba apenas o tamanho do array de Livro por parâmetro e inicialize o array com este tamanho.
+    // Não inclua livros no array, ele deve permanecer vazio no momento da criação da biblioteca.
+
+
+    public Biblioteca( int tamanho) {
+        this.arrayLivro = new Livro[tamanho];
+    }
+
+    public Livro[] getArrayLivro() {
+        return arrayLivro;
+    }
+
+    public void setArrayLivro(Livro[] arrayLivro) {
+        this.arrayLivro = arrayLivro;
+    }
+
+    @Override
+    public String toString() {
+        return "Biblioteca{" +
+                "arrayLivro=" + Arrays.toString(arrayLivro) +
+                '}';
+    }
+    // B - crie o metodo insereLivro, que recebe um livro e insere na primeira posição disponível do array.
+    //  Caso não seja possível inserir o livro no array, retorne false. Se o livro for inserido, retorne true. */
+
+    public boolean insereLivro( Livro livro){
+        for ( int i = 0; i < arrayLivro.length; i++){
+            if ( arrayLivro[i] == null){
+                arrayLivro[i] = livro;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // C - crie o metodo procuraLivroPorTitulo, que recebe uma String titulo e retorna um objeto do tipo Livro,
+    // que é o livro com aquele título. O metodo retorna null caso não haja o livro solicitado na biblioteca.
+
+    public Livro procuraLivroPorTitulo( String titulo){
+        for ( int i = 0; i < arrayLivro.length; i++){
+            if (arrayLivro[i] != null) {
+                if ( arrayLivro[i].getTitulo().equals(titulo)){
+                    return arrayLivro[i];
+                }
+
+            }
+        }
+        return null;
+    }
+
+}
