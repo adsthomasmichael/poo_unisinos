3bbd981d46b42e164ea6e74838c7baa2df4270c7
adsthomasmichael <ads.thomasmichael@gmail.com>
2026-05-15 10:56:24 -0300

    Classe Antigo

---
+++ src/aula012/Antigo.java
@@ -0,0 +1,37 @@
+package aula012;
+
+public class Antigo extends Livro{
+    //Crie a classe Antigo, que é um tipo de Livro e possui um número de edição. Crie o construtores e métodos necessários. Crie o método toString.
+
+    protected int numEdicao;
+
+    //Construtores
+    public Antigo() {
+        super();
+    }
+
+    public Antigo(String titulo, String autor, double proce, int ano, int numEdicao) {
+        super(titulo, autor, proce, ano);
+        this.numEdicao = numEdicao;
+    }
+
+    //Metodos GET e SET
+    public int getNumEdicao() {
+        return numEdicao;
+    }
+
+    public void setNumEdicao(int numEdicao) {
+        this.numEdicao = numEdicao;
+    }
+
+    @Override
+    public String toString() {
+        return "Antigo{" +
+                "numEdicao=" + numEdicao +
+                ", titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", proce=" + proce +
+                ", ano=" + ano +
+                '}';
+    }
+}
