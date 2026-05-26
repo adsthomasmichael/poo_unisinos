8cb3fd2fb0a112cfb275f5542fb5eb3b06e9da2f
adsthomasmichael <ads.thomasmichael@gmail.com>
2026-05-26 17:18:17 -0300

    classe Federal

---
+++ src/aula013_TesteGB/Federal.java
@@ -0,0 +1,25 @@
+package aula013_TesteGB;
+
+public class Federal extends Deputado{
+    //Crie uma classe Federal, que é um tipo de Deputado. Um Deputado Federal possui, além
+    //dos demais atributos, um número de cadastro. Crie os métodos de acesso e construtor necessários.
+
+    protected int numCadastro;
+
+    public Federal(int numCadastro) {
+        this.numCadastro = numCadastro;
+    }
+
+    public Federal(String nome, double salarioBruto, double salarioLiquido, int qtdFuncionario, int numCadastro) {
+        super(nome, salarioBruto, salarioLiquido, qtdFuncionario);
+        this.numCadastro = numCadastro;
+    }
+
+    public int getNumCadastro() {
+        return numCadastro;
+    }
+
+    public void setNumCadastro(int numCadastro) {
+        this.numCadastro = numCadastro;
+    }
+}
