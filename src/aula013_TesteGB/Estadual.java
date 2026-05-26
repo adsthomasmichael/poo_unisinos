7f7071c71626707a97ab5cd234914eb2cd528a3b
adsthomasmichael <ads.thomasmichael@gmail.com>
2026-05-26 17:20:32 -0300

    classe Estadual

---
+++ src/aula013_TesteGB/Estadual.java
@@ -0,0 +1,25 @@
+package aula013_TesteGB;
+
+public class Estadual extends Deputado{
+    //Crie uma classe Estadual, que é um tipo de Deputado. Um Deputado Estadual possui, além
+    //dos demais atributos, um estado. Crie os métodos de acesso e construtor necessários.
+
+    protected String estado;
+
+    public Estadual(String estado) {
+        this.estado = estado;
+    }
+
+    public Estadual(String nome, double salarioBruto, double salarioLiquido, int qtdFuncionario, String estado) {
+        super(nome, salarioBruto, salarioLiquido, qtdFuncionario);
+        this.estado = estado;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
