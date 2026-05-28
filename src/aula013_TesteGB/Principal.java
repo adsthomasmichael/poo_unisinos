115a1331399d5f27a1a2744db82fa3b906352b74
adsthomasmichael <ads.thomasmichael@gmail.com>
2026-05-28 17:15:45 -0300

    correção classe Principal

--- src/aula013_TesteGB/Principal.java
+++ src/aula013_TesteGB/Principal.java
@@ -28,36 +28,112 @@ public class Principal {
     /* B -  imprima o número de cadastro (se for Federal) ou o estado (se for Estadual) de
             todos os deputados corruptos, separados por tipo de corrupção (mais funcionários, que recebem
             valores ou desviam verbas). */
-        for (int i = 0; i < deputados01.length; i++){
-            ProcuraCorruptos pc01 = new ProcuraCorruptos(deputados01);
-            if (pc01.deputadosDesviamVerbas() != null ){
-                System.out.println("=== Desviam verbas ===");
-                    if (deputados01[i] instanceof Federal){//se for Federal, imprimir o num Cadastro
-                        System.out.println("Dep Federal: "+ ((Federal) deputados01[i]).getNome() + " - " +((Federal) deputados01[i]).getNumCadastro());
-                    }
-                    else if (deputados01[i] instanceof  Estadual) { //se for Estadual, imprimir o Estado
-                        System.out.println("Dep Estadual: "+ ((Estadual) deputados01[i]).getNome()+ " - "+ ((Estadual) deputados01[i]).getEstado());
-                    }
-            }
-            if (pc01.deputadosMaisFuncionarios() != null){
-                System.out.println("=== Excesso de funcionários ===");
-                    if (deputados01[i] instanceof Federal){//se for Federal, imprimir o num Cadastro
-                        System.out.println("Dep Federal: "+ ((Federal) deputados01[i]).getNome() + " - " +((Federal) deputados01[i]).getNumCadastro());
-                    }
-                    else if (deputados01[i] instanceof  Estadual) { //se for Estadual, imprimir o Estado
-                        System.out.println("Dep Estadual: "+ ((Estadual) deputados01[i]).getNome()+ " - "+ ((Estadual) deputados01[i]).getEstado());
-                    }
-            }
-            if (pc01.deputadosRecebemValores() != null){
-                System.out.println("=== Recebem valores ===");
-                    if (deputados01[i] instanceof Federal){//se for Federal, imprimir o num Cadastro
-                        System.out.println("Dep Federal: "+ ((Federal) deputados01[i]).getNome() + " - " +((Federal) deputados01[i]).getNumCadastro());
-                    }
-                    else if (deputados01[i] instanceof  Estadual) { //se for Estadual, imprimir o Estado
-                        System.out.println("Dep Estadual: "+ ((Estadual) deputados01[i]).getNome()+ " - "+ ((Estadual) deputados01[i]).getEstado());
-                    }
-            }
+        ProcuraCorruptos pc01 =
+                new ProcuraCorruptos(deputados01);
 
+
+// ================= DESVIAM VERBAS =================
+
+        System.out.println("=== Desviam verbas ===");
+
+        Deputado[] desvios =
+                pc01.deputadosDesviamVerbas();
+
+        for (int i = 0; i < desvios.length; i++) {
+
+            if (desvios[i] != null) {
+
+                if (desvios[i] instanceof Federal) {
+
+                    Federal f = (Federal) desvios[i];
+
+                    System.out.println(
+                            "Federal: "
+                                    + f.getNome()
+                                    + " - Cadastro: "
+                                    + f.getNumCadastro());
+
+                } else {
+
+                    Estadual e = (Estadual) desvios[i];
+
+                    System.out.println(
+                            "Estadual: "
+                                    + e.getNome()
+                                    + " - Estado: "
+                                    + e.getEstado());
+                }
+            }
+        }
+
+
+// ================= MAIS FUNCIONÁRIOS =================
+
+        System.out.println("\n=== Mais funcionários ===");
+
+        Deputado[] funcionarios =
+                pc01.deputadosMaisFuncionarios();
+
+        for (int i = 0; i < funcionarios.length; i++) {
+
+            if (funcionarios[i] != null) {
+
+                if (funcionarios[i] instanceof Federal) {
+
+                    Federal f = (Federal) funcionarios[i];
+
+                    System.out.println(
+                            "Federal: "
+                                    + f.getNome()
+                                    + " - Cadastro: "
+                                    + f.getNumCadastro());
+
+                } else {
+
+                    Estadual e = (Estadual) funcionarios[i];
+
+                    System.out.println(
+                            "Estadual: "
+                                    + e.getNome()
+                                    + " - Estado: "
+                                    + e.getEstado());
+                }
+            }
+        }
+
+
+// ================= RECEBEM VALORES =================
+
+        System.out.println("\n=== Recebem valores ===");
+
+        Deputado[] recebem =
+                pc01.deputadosRecebemValores();
+
+        for (int i = 0; i < recebem.length; i++) {
+
+            if (recebem[i] != null) {
+
+                if (recebem[i] instanceof Federal) {
+
+                    Federal f = (Federal) recebem[i];
+
+                    System.out.println(
+                            "Federal: "
+                                    + f.getNome()
+                                    + " - Cadastro: "
+                                    + f.getNumCadastro());
+
+                } else {
+
+                    Estadual e = (Estadual) recebem[i];
+
+                    System.out.println(
+                            "Estadual: "
+                                    + e.getNome()
+                                    + " - Estado: "
+                                    + e.getEstado());
+                }
+            }
         }
 
 
