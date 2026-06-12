9083aab7901a20c08338965a54a22acb06ba8c8a
adsthomasmichael <ads.thomasmichael@gmail.com>
2026-06-12 18:46:08 -0300

    Pacote aula016_revisãoProvaGB e classe Funcionario

---
+++ src/aula016_revisaoProvaGB/Funcionario.java
@@ -0,0 +1,52 @@
+package aula016_revisaoProvaGB;
+
+/* Crie uma classe chamada Funcionario. Um funcionário possui um nome, um
+salário bruto e um salário líquido. Crie os métodos de acesso e o metodo construtor.*/
+
+public class Funcionario {
+
+    protected String nome;
+    protected double salarioBruto;
+    protected double salarioLiquido;
+
+
+    public Funcionario() {
+    }
+
+    public Funcionario(String nome, double salarioBruto, double salarioLiquido) {
+        this.nome = nome;
+        this.salarioBruto = salarioBruto;
+        this.salarioLiquido = salarioLiquido;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getSalarioBruto() {
+        return salarioBruto;
+    }
+
+    public void setSalarioBruto(double salarioBruto) {
+        this.salarioBruto = salarioBruto;
+    }
+
+    public double getSalarioLiquido() {
+        return salarioLiquido;
+    }
+
+    public void setSalarioLiquido(double salarioLiquido) {
+        this.salarioLiquido = salarioLiquido;
+    }
+
+
+
+}
+
+
+
+
